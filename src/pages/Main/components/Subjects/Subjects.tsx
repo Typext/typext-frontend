eb3658a3378e88dc345e9bc5e8fee726d63ebe43
MatheusCampos-450 <matheus.campos14@fatec.sp.gov.br>
2021-03-26 22:17:05 -0300

    initial devolpment subjects component

---
+++ src/pages/Main/components/Subjects/Subjects.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Subjects = () => {
+  return <h1>teste</h1>;
+};
+
+export default Subjects;
---
+++ src/pages/Main/components/Subjects/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledSubjects = styled.div`
+  display: flex;
+`;
+
+export default StyledSubjects;
