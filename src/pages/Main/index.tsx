f6e2059dcc7927b81b73945ea8f1fdd579543fe0
leonardomessias98 <messiasleonardo98@gmail.com>
2021-03-27 11:15:56 -0300

    adjusts

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,13 +2,12 @@ import React, { useState } from 'react';
 import Initial from './components/Initial/Initial';
 import ProjectInformation from './components/ProjectInformation/ProjectInformation';
 import OptionButtons from './components/OptionButtons';
-<<<<<<< HEAD
 import Topics from './components/Topics/Topics';
-=======
 import Subjects from './components/Subjects/Subjects';
 import Distributions from './components/Distributions/Distributions';
+
+//eslint-disabled-next-line
 import MinuteViewer from './components/MinuteViewer';
->>>>>>> 41e3279a1ceed5e8baf2d6d863515f8bd5ca281f
 
 import Container from './styles';
 
@@ -22,13 +21,10 @@ const Main = () => {
       <Container>
         <Initial />
         <ProjectInformation />
-<<<<<<< HEAD
         <Topics />
 
-=======
         <Subjects />
         <Distributions />
->>>>>>> 41e3279a1ceed5e8baf2d6d863515f8bd5ca281f
         <OptionButtons setShowMinute={setShowMinute} />
       </Container>
     </>
