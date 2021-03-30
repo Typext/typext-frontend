c252240c2244112d5b6152eac9d3921ffac4f56d
Henrique Sousa <washington.sousa01@fatec.sp.gov.br>
2021-03-30 19:48:23 -0300

    implementing login screen

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import Main from './pages/Main';
+import Login from './pages/Login';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
+        <Route path="/login" exact component={Login} />
       </Switch>
     </BrowserRouter>
   );
