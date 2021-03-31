d5b96f58db400001e6b3298c85e0a98d6496e22f
Henrique Sousa <washington.sousa01@fatec.sp.gov.br>
2021-03-31 15:19:00 -0300

    NewPassword Screen Front-end

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import RecoveryPassword from 'pages/RecoveryPassword';
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
+import NewPassword from 'pages/NewPassword';
 import Main from './pages/Main';
 
 export default function Routes() {
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Main} />
         <Route path="/recovery-password" exact component={RecoveryPassword} />
+        <Route path="/new-password" exact component={NewPassword} />
       </Switch>
     </BrowserRouter>
   );
