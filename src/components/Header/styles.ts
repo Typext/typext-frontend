2159f3f6223dfc68a963d29316c937fbcb9295c3
leonardomessias98 <messiasleonardo98@gmail.com>
2021-04-01 23:49:31 -0300

    adjusting header style

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -38,4 +38,10 @@ export const StyledHeader = styled.header`
     border: none;
     background: none;
   }
+
+  .shortOptions {
+    max-width: 150px;
+    width: 100%;
+    justify-content: flex-start;
+  }
 `;
