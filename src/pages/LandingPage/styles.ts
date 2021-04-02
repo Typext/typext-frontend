45e05e11fbd8194607d2eb5880563868a134cc06
leonardomessias98 <messiasleonardo98@gmail.com>
2021-04-01 23:42:09 -0300

    hot fix in landing page style

--- src/pages/LandingPage/styles.ts
+++ src/pages/LandingPage/styles.ts
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+
   svg {
     max-width: 40rem;
     width: 100%;
