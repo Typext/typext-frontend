b233fbc0c665e28664a9e813cd2b1032000aa5de
MatheusCampos-450 <matheus.campos14@fatec.sp.gov.br>
2021-04-05 14:40:19 -0300

    initial commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+
+import { Calendar, Badge } from 'antd';
+
 import { useHistory } from 'react-router-dom';
 
+import Button from 'components/Button/Button';
 import { Container } from './styles';
 
 const Home = () => {
@@ -12,10 +16,46 @@ const Home = () => {
 
   return (
     <Container>
-      Home asd123 12312
-      <button type="button" onClick={handleNavigate}>
-        Teste
-      </button>
+      <div className="Home">
+        <div className="Calendar">
+          <h1>Agenda</h1>
+          <Calendar />
+        </div>
+
+        <div className="Buttons">
+          <Button
+            color="var(--red-pink)"
+            colorText="white"
+            size="33.75rem"
+          >
+            Criar ATA
+          </Button>
+
+          <Button
+            color="var(--gray)"
+            colorText="var(--black)"
+            size="33.75rem"
+          >
+            Convidar
+          </Button>
+
+          <Button
+            color="var(--gray)"
+            colorText="var(--black)"
+            size="33.75rem"
+          >
+            Buscar ATAS
+          </Button>
+
+          <Button
+            color="var(--gray)"
+            colorText="var(--black)"
+            size="33.75rem"
+          >
+            Lista de Usuários
+          </Button>
+        </div>
+      </div>
     </Container>
   );
 };
