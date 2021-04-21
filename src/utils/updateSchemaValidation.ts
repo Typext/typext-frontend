0dd4857399da258bbbcaef51b69846a8acc76c00
leonardo messias <messiasleonardo98@gmail.com>
2021-04-21 16:02:22 -0300

    fix(phone input): preventing user type alphabetic caracter in phone input

--- src/utils/updateSchemaValidation.ts
+++ src/utils/updateSchemaValidation.ts
@@ -1,9 +1,13 @@
 import * as Yup from 'yup';
 
 export default {
-  name: Yup.string().required('Nome obrigatório'),
+  name: Yup.string()
+    .max(100, 'O nome pode conter no máximo 100 dígitos')
+    .required('Nome obrigatório'),
   office: Yup.string().required('Cargo obrigatório'),
   area: Yup.string().required('Área obrigatória'),
-  phone: Yup.string().required('Telefone obrigatório'),
+  phone: Yup.string()
+    .max(15, 'O número deve conter no máximo 15 dígitos')
+    .required('Telefone obrigatório'),
   company: Yup.string().required('Empresa obrigatória'),
 };
